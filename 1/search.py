51cda5be5d1cb96df23d53fa82bc933dda7db251
Tyler Tracy <me@tylertracy.com>
2019-09-20 12:13:47 -0500

    Fixed ucs and a* to use correct seen set

--- 1/search.py
+++ 1/search.py
@@ -22,7 +22,7 @@ def prettyPrint(path, nodesVisited):
     print("Num nodes visited: " + str(nodesVisited))
     print("Num of nodes on path: " + str(len(path)))
     dist = 0
-    for i in range(0, len(path) - 2):
+    for i in range(0, len(path) - 1):
         dist += graph[path[i]][path[i+1]]
     print("Distance (km): " + str(dist)) 
 
@@ -45,43 +45,43 @@ def bfs(startNode, endNode):
     return False
 
 def ucs(startNode, endNode):
-    queue = [(0, [startNode])]
+    queue = [(0, [startNode], startNode)]
     seen = set()
     numOfNodesVisited = 0
     while len(queue) > 0:
+        weight, path, current = heappop(queue)
+        if current in seen: continue
+        seen.add(current)
         numOfNodesVisited += 1
-        weight, path = heappop(queue)
-        checking = path[len(path) - 1]
-        for neighbor in graph[checking]:
+        for neighbor in graph[current]:
             if neighbor == endNode:
                 path.append(neighbor)
                 return (path, numOfNodesVisited)
             if not neighbor in seen:
-                newWeight = weight + graph[checking][neighbor]
+                newWeight = weight + graph[current][neighbor]
                 newPath = [*path, neighbor]
-                seen.add(neighbor)
-                heappush(queue, (newWeight, newPath))
+                heappush(queue, (newWeight, newPath, neighbor))
     return False
 
 def astar(startNode, endNode):
-    queue = [(heur[startNode],[startNode])]
+    queue = [(heur[startNode],[startNode], startNode)]
     seen = set()
     numOfNodesVisited = 0
     while len(queue) > 0:
+        score, path, current = heappop(queue)
+        if current in seen:
+            continue
         numOfNodesVisited += 1
-        score, path = heappop(queue)
-        current = path[len(path) - 1]
+        seen.add(current)
         score -= heur[current] # remove the last heuristic
         for neighbor in graph[current]:
             if neighbor == endNode:
                 path.append(neighbor)
                 return (path, numOfNodesVisited)
             if not neighbor in seen:
-                # remove the last heuristic
                 newScore = score + heur[neighbor] + graph[current][neighbor]
                 newPath = [*path, neighbor]
-                heappush(queue, (newScore, newPath))
-                seen.add(neighbor)
+                heappush(queue, (newScore, newPath, neighbor))
     return False
 
 print("\nBFS:")
