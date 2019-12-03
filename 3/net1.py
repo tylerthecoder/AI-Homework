b7313ca307f0ebb50d7fcd164db5048f73a1238c
City303 <City303@users.noreply.github.com>
2019-12-03 09:40:01 -0600

    Linting

--- 3/net1.py
+++ 3/net1.py
@@ -19,14 +19,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
+
+'''
+Constants for the net, training, and testing
+'''
 BATCH_SZ   = 4    # Batch size
+
 DIM_IN     = 2    # Input dimension          (2, for two XOR inputs)
-DIM_H      = 16    # Hidden layer dimensions
+DIM_H      = 16   # Hidden layer dimensions
 DIM_OUT    = 2    # Output dimension         (2, for XOR output of 0 (index 0) or 1 (index 1) - one hot classification)
-LEARN_RATE = 0.1  # Learning rate of NN
+
+LEARN_RATE = 0.1   # Learning rate of NN
 EPOCHS     = 1000  # Maximum allowed number of training iterations for NN
 
 
+
+'''
+Class definition of the XOR neural net
+'''
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
@@ -42,7 +53,10 @@ class Net(nn.Module):
         return x
 
 
-# Train the neural net
+
+'''
+Train the neural net (with backpropagation)
+'''
 def train(net, train_set):
     optimizer = optim.Adam(net.parameters(), lr=LEARN_RATE)
     loss = torch.tensor([1])
@@ -73,7 +87,11 @@ def train(net, train_set):
 
     print(f'Epoch #{str(epoch+1).ljust(2)} loss: {round(loss.item(), 3)}')
 
-# Test the neural net
+
+
+'''
+Basic test of the neural net
+'''
 def test(net, test_set):
 
     print('\nTest output:')
@@ -90,21 +108,27 @@ def test(net, test_set):
             print()
 
 
-# The actual code (not functions) begins here
+
+'''
+TODO test again and graph using MatPlotLib
+'''
+# def fancy_test(net):
+
+
+
+'''
+Actual code (not functions) begins here
+'''
 test_set  = torch.tensor([[0,0,1,0], [0,1,0,1], [1,0,0,1], [1,1,1,0]], dtype=torch.float32)
 train_set = torch.tensor([[0,0,1,0], [0,1,0,1], [1,0,0,1], [1,1,1,0]], dtype=torch.float32)
 # np.random.shuffle(train_set) # TODO it is not shuffling the values correctly for some reason
 
-print('\ntest_set:\n', test_set)
-print('\ntrain_set:\n', train_set) 
-
-
 net = Net()
 
 print('\nNet: ', net)
+print('\nTraining Set:\n', train_set) 
+print('\nTesting Set:\n', test_set)
 
 train(net, train_set)
 test(net, test_set)
-
-
-# def print_results():
+# fancy_test(net)
--- 3/net2.py
+++ 3/net2.py
@@ -18,27 +18,39 @@ Input | Expected output
  etc.    etc.
 '''
 
-import numpy as np     # Numpy dependency
+import numpy as np     # Numpy dependency (CHECK IF USED)
 import torch           # Pytorch dependenices
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import random          # Python dependency
 
-BATCH_SZ   = 10    # Batch size
-DIM_IN     = 1     # Input dimension          (1, for the value of x)
-DIM_H      = 8     # Hidden layer dimensions
-DIM_OUT    = 1     # Output dimension         (1, for the value of y = x^2)
+
+
+'''
+Constants for the net, training, and testing
+'''
+TRAIN_BATCH_SZ = 200   # Batch size for train / test sets
+TEST_BATCH_SZ  = 10
+MAX_X_VALUE    = 50    # Maximum value for x for y = x^2
+
+DIM_IN     = 1     # Input dimension   (1, for the value of x)
+DIM_OUT    = 1     # Output dimension  (1, for the value of y = x^2)
+
 LEARN_RATE = 0.1   # Learning rate of NN
-EPOCHS     = 20    # Maximum allowed number of training iterations for NN
+EPOCHS     = 15    # Maximum allowed number of training iterations for NN
 
 
+
+'''
+Class definition of the x^2 neural net
+'''
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(DIM_IN, 4)  # Input layer
-        self.fc2 = nn.Linear(4, 8)   # Hidden layer
-        self.fc3 = nn.Linear(8, 8)   # Hidden layer
+        self.fc2 = nn.Linear(4, 8)       # Hidden layer
+        self.fc3 = nn.Linear(8, 8)       # Hidden layer
         self.fc4 = nn.Linear(8, DIM_OUT) # Output layer
 
     def forward(self, x):
@@ -47,12 +59,15 @@ class Net(nn.Module):
         x = torch.relu(self.fc1(x)) # Forward pass through all the layers
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = self.fc4(x)            # Don't want to ReLU the last layer
+        x = self.fc4(x)             # Don't want to ReLU the last layer
         return x
         #return F.log_softmax(x, dim=1)
 
 
-# Train the neural net
+
+'''
+Train the neural net (with backpropagation)
+'''
 def train(net, train_set):
     optimizer = optim.Adam(net.parameters(), lr=LEARN_RATE)
     loss  = torch.tensor([1])
@@ -68,7 +83,7 @@ def train(net, train_set):
             # print('sample:', sample)
             # print('label :', label)
 
-            output = net(sample.view(-1, 1))      # Run the NN on the value
+            output = net(sample.view(-1, 1))            # Run the NN on the value
             loss   = F.smooth_l1_loss(output[0], label) # Calculate how incorrect the NN was
 
             optimizer.zero_grad() # Start gradient at zero
@@ -83,7 +98,11 @@ def train(net, train_set):
 
     # print(f'Epoch #{str(epoch).ljust(2)} loss: {round(loss.item(), 3)}')
 
-# Test the neural net
+
+
+'''
+Basic test of the neural net
+'''
 def test(net, test_set):
 
     running_diff = 0
@@ -91,7 +110,7 @@ def test(net, test_set):
     print('\nTest output:')
     with torch.no_grad():
         for data in test_set:
-            # print('data:', data)
+
             sample = data[0]
             label  = data[1]
             output = net(sample.view(-1, DIM_IN))
@@ -105,8 +124,19 @@ def test(net, test_set):
     
     print(f'\nAvg difference: {round(running_diff/len(test_set), 3)}%')
 
-# Generates a batch_size long tensor with the values
-# for x and x^2, with the range for x being [-max_x, max_x]
+
+
+'''
+TODO test again and graph using MatPlotLib
+'''
+# def fancy_test(net):
+
+
+
+'''
+Generates a batch_size long tensor with the values
+for x and x^2, with the range for x being [-max_x, max_x]
+'''
 def generate_set(batch_size, max_x):
     RAND_STEP = 0.001 # The granularity of the values in the test set
     set = torch.zeros(batch_size, 2, dtype=torch.float32)
@@ -121,11 +151,12 @@ def generate_set(batch_size, max_x):
     return set
 
 
-# The actual code (not functions) begins here
 
-train_set = generate_set(200, 50)
-test_set  = generate_set(10, 50)
-
+'''
+Actual code (not functions) begins here
+'''
+train_set = generate_set(TRAIN_BATCH_SZ, MAX_X_VALUE)
+test_set  = generate_set(TEST_BATCH_SZ, MAX_X_VALUE)
 
 net = Net()
 
@@ -135,6 +166,5 @@ print('\nTesting Set: ', set)
 
 train(net, train_set)
 test(net, test_set)
+# fancy_test(net)
 
-
-# def print_results():
